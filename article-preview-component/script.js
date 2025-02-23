5b8f5284bea430ed2dfb67c8fb882812b7d42f06
Francine <francine.godoi@gmail.com>
2025-02-23 19:25:33 -0300

    added share options popup with js

---
+++ article-preview-component/script.js
@@ -0,0 +1,12 @@
+function showShareBtn(){
+    let shareIcons = document.getElementsByClassName('share-icons')
+    let arrowIcon = document.getElementsByClassName('share')
+
+    if (arrowIcon[0].classList.contains('share-active')){
+        shareIcons[0].classList.remove('share-icons-show')    
+        arrowIcon[0].classList.remove('share-active')
+    } else {
+        shareIcons[0].classList.add('share-icons-show')    
+        arrowIcon[0].classList.add('share-active')
+    }
+}
