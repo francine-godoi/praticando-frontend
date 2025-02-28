031207e898cc55c0f654b91544db4bdf913efa9e
Francine <francine.godoi@gmail.com>
2025-02-28 11:28:52 -0300

    finalizado html e css versão desktop

---
+++ time-tracking-dashboard/design/script.js
@@ -0,0 +1,6 @@
+
+// TODO: grid responsivo, max 4 colunas min 1
+//       versão mobile
+//       envolver horas/previous em div para faciliar versão mobile
+//       filtro usando js e JSON
+//       filtro ativo com enfase
