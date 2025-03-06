bd0f6171e8b6e8284eb1556aaafcb2807553af07
Francine <francine.godoi@gmail.com>
2025-03-06 15:18:04 -0300

    iniciado html

---
+++ tip-calculator-app/script.js
@@ -0,0 +1,6 @@
+/*
+TODO:
+    - tirar seleção dos checkbox quando clicar em custom
+    - clicar clickar em outro checkbox, zera custom
+    - eventListner para verificar se todas as opções foram selecionadas para calcular tip
+*/
