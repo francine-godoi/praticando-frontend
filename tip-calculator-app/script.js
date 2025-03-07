e4f9f6fa12055bbfb283d7e99265961d8fdc7bec
Francine <francine.godoi@gmail.com>
2025-03-07 14:19:42 -0300

    link js na pagina

--- tip-calculator-app/script.js
+++ tip-calculator-app/script.js
@@ -4,4 +4,8 @@ TODO:
     - tirar seleção dos checkbox quando clicar em custom
     - clicar clickar em outro checkbox, zera custom
     - eventListner para verificar se todas as opções foram selecionadas para calcular tip
-*/
+    
+    function clearRadioBtn() {
+        document.querySelector('input[name="tip"]:checked').checked = false;
+    }
+    */
