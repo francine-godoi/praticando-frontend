ae8047fa504b50cd5c360d0270fe39be8978e2a2
Francine <francine.godoi@gmail.com>
2025-03-14 14:18:16 -0300

    added success message style

---
+++ contact-form/script.js
@@ -0,0 +1,7 @@
+/* TODO:
+    - add * to fields
+    - js to add "checked-radio" class to selected radiobutton
+    - mobile version
+    - show required message for empty fields
+    - make success message desapear after a few secs
+*/
