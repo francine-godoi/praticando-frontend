9e73f5cb3436b0a1572567b48dd9af5f32a84944
Francine <francine.godoi@gmail.com>
2025-03-14 09:16:00 -0300

    fixed accordion to be accessible via keyboard

--- faq-accordion/script.js
+++ faq-accordion/script.js
@@ -2,19 +2,8 @@ accordion = document.getElementsByClassName("title-question");
 
 for (let i = 0; i < accordion.length; i++) {
     accordion[i].addEventListener("click", () => {
-        accordion[i].nextElementSibling.classList.toggle("open");
-        accordion[i].children[1].classList.toggle("inactive");
-        accordion[i].children[2].classList.toggle("open");
+        accordion[i].nextElementSibling.classList.toggle("open"); /* answer */
+        accordion[i].children[1].classList.toggle("inactive"); /* plus sign */
+        accordion[i].children[2].classList.toggle("open"); /* minus sign */
     });
 }
-
-/* 
-    TODO: remake accordion
-
-    https://www.aditus.io/patterns/accordion/
-
-    Accessible Accordion
-
-    Navigate the questions and hide/show answers using keyboard navigation alone
-
-*/
