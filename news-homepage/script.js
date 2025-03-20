3f75089c6571a69ea75901f56c53538a8404dd75
Francine <francine.godoi@gmail.com>
2025-03-20 17:30:42 -0300

    dim background mobile menu active

--- news-homepage/script.js
+++ news-homepage/script.js
@@ -1,15 +1,16 @@
 const burger = document.getElementById("burger");
 const navLinks = document.getElementById("nav-links");
 const closeBtn = document.getElementById("close");
+const dim = document.getElementById("dim");
 
 burger.addEventListener("click", () => {
     navLinks.classList.add("mobile");
     closeBtn.style.display = "block";
+    dim.classList.add("dim");
 });
 
 closeBtn.addEventListener("click", () => {
     navLinks.classList.remove("mobile");
     closeBtn.style.display = "none";
+    dim.classList.remove("dim");
 });
-
-// TODO: mudar o fundo quando menu aberto
