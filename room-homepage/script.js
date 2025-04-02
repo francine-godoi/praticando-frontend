7563b6778fa9ea9a91cea60e3e60496b12990eed
Francine <francine.godoi@gmail.com>
2025-04-02 14:10:29 -0300

    removido linhas não usadas

--- room-homepage/script.js
+++ room-homepage/script.js
@@ -1,12 +1,9 @@
-function openMenu(){
-    document.getElementById('nav-links').classList.add('mobile-nav-links')
-    document.getElementById('close-menu').classList.remove('inactive')
-    document.getElementById('mobile-menu-icon').classList.add('inactive')
-    
+function openMenu() {
+  document.getElementById("nav-links").classList.add("mobile-nav-links");
+  document.getElementById("close-menu").classList.remove("inactive");
 }
 
-function closeMenu(){
-    document.getElementById('nav-links').classList.remove('mobile-nav-links')
-    document.getElementById('close-menu').classList.add('inactive')
-    document.getElementById('mobile-menu-icon').classList.remove('inactive')
+function closeMenu() {
+  document.getElementById("nav-links").classList.remove("mobile-nav-links");
+  document.getElementById("close-menu").classList.add("inactive");
 }
